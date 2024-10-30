d9d80bcaa085832647a71e4a15aca03bc6eb9ca1
Davi Aquino <djejaquino@gmail.com>
2024-10-30 17:28:56 -0300

    test: make signature static

--- authenticate.test.js
+++ authenticate.test.js
@@ -4,17 +4,21 @@ describe('authenticate', () => {
   test('should add Authentication header', async () => {
     const { headers } = await authenticate({
       args: {
-        headers: {},
+        headers: {
+          // making sure headers casing doesn't matter
+          'x-DaTe': '2024-10-30T20:05:21.188Z',
+          'X-lOgIn': '0123456789',
+        },
         body: {
           foo: 'bar',
         },
       },
       configuration: {
-        DLOCAL_SECRET_KEY: '2P6GBSQ8ZTZLP3MZ98SZ',
+        DLOCAL_SECRET_KEY: 'ABCDEFG123456789',
       },
     });
-    expect(headers.Authorization).toMatch(
-      /^V2-HMAC-SHA256, Signature:\s[a-z0-9]{64}$/gu
+    expect(headers.Authorization).toStrictEqual(
+      'V2-HMAC-SHA256, Signature: b809e7e2e21f895863997bb2bb3751f02906aceac4fdc6789c29646b7aff9a85'
     );
   });
 });
